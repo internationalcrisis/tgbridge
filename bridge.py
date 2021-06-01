c1c9f68f18e8aa13ec87fc38b19311b42a4d0bc3
Nuclear Winter <admin@ccrda.us>
2021-06-01 04:34:14 -0500

    Bridge uses config.yml for configuration data rather than hard-coding values.

--- bridge.py
+++ bridge.py
@@ -3,20 +3,22 @@ import telethon
 from discord import Webhook, AsyncWebhookAdapter
 from pprint import pprint
 
+import yaml
 import aiohttp
 import os
 import logging
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
+config = yaml.safe_load(open('config.yml'))
+
 # Use your own values from my.telegram.org
 api_id = ***REMOVED***
 api_hash = '***REMOVED***'
 
-webbase = "https://ccrda.us/tgbridge/" # TODO: handle trailing slash
-filebase = "files/" # TODO: handle trailing slash
+config['telegram']['base_url'] = "https://ccrda.us/tgbridge/" # TODO: handle trailing slash
+config['telegram']['files_dir'] = "files/" # TODO: handle trailing slash
 
-webhookurl = '***REMOVED***'
 
 loggedchats = [
     ***REMOVED***
@@ -27,16 +29,16 @@ loggedchats = [
 ]
 
 # The first parameter is the .session file name (absolute paths allowed)
-client = TelegramClient('sus', api_id, api_hash)
+client = TelegramClient(config['telegram']['sessionfile'], config['telegram']['api_id'], config['telegram']['api_hash'])
 
-@client.on(events.NewMessage(chats=loggedchats))
+@client.on(events.NewMessage(chats=config['telegram']['watched']))
 async def on_message(event):
     async with aiohttp.ClientSession() as session:
         webhookmsg = ''
 
         ifp = f"{event.chat.id}.jpg"
         if not os.path.exists(ifp) and not isinstance(event.chat.photo, telethon.types.ChatPhotoEmpty):
-            icon = await client.download_profile_photo(event.chat, file=filebase+ifp)
+            await client.download_profile_photo(event.chat, file=config['telegram']['files_dir']+ifp)
 
         if event.message.forward:
             try:
@@ -59,20 +61,20 @@ async def on_message(event):
 
         if event.message.file and not event.message.web_preview:
             mfp = f'{event.chat.id}-{event.message.id}{event.message.file.ext}'
-            with open(filebase+mfp, 'wb') as f:
+            with open(config['telegram']['files_dir']+mfp, 'wb') as f:
                 async for chunk in client.iter_download(event.message.file.media):
                     f.write(chunk)
             
-            webhookmsg += f'\n\n{webbase}{mfp}'
+            webhookmsg += f'\n\n{config["telegram"]["base_url"]}{mfp}'
 
-        webhook = Webhook.from_url(webhookurl, adapter=AsyncWebhookAdapter(session))
+        webhook = Webhook.from_url(config['discord']['webhookurl'], adapter=AsyncWebhookAdapter(session))
         # coroutine to get self is in parenthesis so .id runs on the result of awaiting it
         if not isinstance(event.chat, telethon.types.User):
-            await webhook.send(webhookmsg, username=f'{event.chat.title}', avatar_url=webbase+ifp)
+            await webhook.send(webhookmsg, username=f'{event.chat.title}', avatar_url=config['telegram']['base_url']+ifp)
         elif (await client.get_me()).id == event.chat.id: 
-            await webhook.send(webhookmsg, username=f'Saved Messages', avatar_url=webbase+ifp)
+            await webhook.send(webhookmsg, username=f'Saved Messages', avatar_url=config['telegram']['base_url']+ifp)
         elif isinstance(event.chat, telethon.types.User):
-            await webhook.send(webhookmsg, username=f'DM with {event.chat.first_name} {event.chat.last_name}', avatar_url=webbase+ifp)
+            await webhook.send(webhookmsg, username=f'DM with {event.chat.first_name} {event.chat.last_name}', avatar_url=config['telegram']['base_url']+ifp)
 
 async def main():
     async for dialog in client.iter_dialogs():
